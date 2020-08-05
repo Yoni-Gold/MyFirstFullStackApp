518d0c5a37d8d5e5b7946cc1233424627191b676
alontzukerman <67857535+alontzukerman@users.noreply.github.com>
2020-08-05 19:08:33 +0300

    GET product by id

--- ShoppingListClient.js
+++ ShoppingListClient.js
@@ -7,4 +7,12 @@ productsButtonElement.addEventListener('click', async ()=> {
     console.log(data);
 });
 
+const inputElement = document.querySelector('#inputProduct');
+const searchProductButtonElement = document.querySelector('#searchProduct');
+// get product by id
+searchProductButtonElement.addEventListener('click', async ()=> {
+    let productID = inputElement.value;
+    const { data } = await axios.get(`http://localhost:3000/products/${productID}`);
+    console.log(data);
+});
 

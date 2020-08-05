c7cc5006566b0e3c91ed7e5d70d791ace7ec0a83
alontzukerman <67857535+alontzukerman@users.noreply.github.com>
2020-08-05 19:26:46 +0300

    POST new product

--- ShoppingListClient.js
+++ ShoppingListClient.js
@@ -3,8 +3,8 @@
 const productsButtonElement = document.querySelector('#products');
 // get products list from server
 productsButtonElement.addEventListener('click', async ()=> { 
-    const { data } = await axios.get(`http://localhost:3000/products/`);
-    console.log(data);
+    const response = await axios.get(`http://localhost:3000/products/`);
+    console.log(response.data);
 });
 
 const inputElement = document.querySelector('#inputProduct');
@@ -12,7 +12,19 @@ const searchProductButtonElement = document.querySelector('#searchProduct');
 // get product by id
 searchProductButtonElement.addEventListener('click', async ()=> {
     let productID = inputElement.value;
-    const { data } = await axios.get(`http://localhost:3000/products/${productID}`);
-    console.log(data);
+    const response = await axios.get(`http://localhost:3000/products/${productID}`);
+    console.log(response.data);
+});
+
+const newProductButton = document.querySelector('#newProduct');
+// post new product to the list
+newProductButton.addEventListener('click', async ()=> {
+    let product = {
+        name: 'coffee',
+        amount: '3',
+        price: '10'
+    };
+    const response = await axios.post(`http://localhost:3000/products/`, product);
+    console.log(response.data);
 });
 
